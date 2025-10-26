4091aaeb0067b0346ad9add1af7819103d49d468
Bitto Kazi <bitto.kazi@gmail.com>
2025-10-26 10:41:58 +0100

    release: v1.0.2 🔖 (#10)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,10 @@ plugins {
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
-version = "1.0.1"
+version = "1.0.2"
 group = "com.bittokazi.sonartype"
 
-val resourcesVersion = "1.0.1"
+val resourcesVersion = "1.0.2"
 
 repositories {
     mavenCentral()
